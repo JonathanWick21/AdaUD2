22e1095d2d25296653cf642b3e2ecd461b49b06d
jonvilmor <jonathanwick2110@gmail.com>
2025-10-08 19:07:21 +0200

    fin ejercicio 2.3

--- src/Dao/ClienteDao.java
+++ src/Dao/ClienteDao.java
@@ -1,9 +1,10 @@
 package Dao;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import model.Pedido;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ClienteDao implements AutoCloseable{
     private final Connection connection;
@@ -20,9 +21,33 @@ public class ClienteDao implements AutoCloseable{
         try (PreparedStatement ps = connection.prepareStatement(query)){
             ps.setString(1, newName);
             ps.setInt(2, id);
-            ps.execute();
+            int filas = ps.executeUpdate();
+            return filas == 1;
         }
     }
+
+    public List<Pedido> getPedidosByCliente (int clienteId){
+        List<Pedido> pedidos = new ArrayList<>();
+        String query = "SELECT * FROM pedidos WHERE cliente_id = ? ORDER BY date ASC, id ASC";
+        try (PreparedStatement ps = connection.prepareStatement(query)){
+            ps.setInt(1, clienteId);
+            try (ResultSet rs = ps.executeQuery()){
+                while (rs.next()){
+                    Pedido pedido = new Pedido();
+                    pedido.setId(rs.getInt("id"));
+                    pedido.setClienteId(rs.getInt("id_cliente"));
+                    pedido.setFecha(rs.getDate("date").toLocalDate());
+                    pedido.setTotal(rs.getDouble("total"));
+                    pedidos.add(pedido);
+                }
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return pedidos;
+    }
+
 
 
 
---
+++ src/model/Pedido.java
@@ -0,0 +1,43 @@
+package model;
+
+import java.time.LocalDate;
+
+public class Pedido {
+    private int id;
+    private int clienteId;
+    private LocalDate fecha;
+    private double total;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getClienteId() {
+        return clienteId;
+    }
+
+    public void setClienteId(int clienteId) {
+        this.clienteId = clienteId;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+}
