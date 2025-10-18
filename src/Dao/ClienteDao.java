9c29f5dade2cea6b9bb4f227325586d2852085c7
Jonathan <jonathanwick2110@gmail.com>
2025-10-19 00:29:13 +0200

    fin creo bueno falta el main

--- src/Dao/ClienteDao.java
+++ src/Dao/ClienteDao.java
@@ -1,10 +1,14 @@
 package Dao;
 
+import dto.ResumenCliente;
 import model.Pedido;
 
 import java.sql.*;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class ClienteDao implements AutoCloseable{
     private final Connection connection;
@@ -41,7 +45,6 @@ public class ClienteDao implements AutoCloseable{
                     pedidos.add(pedido);
                 }
             }
-
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -49,6 +52,92 @@ public class ClienteDao implements AutoCloseable{
     }
 
 
+    public void deleteClienteYPedidos(int clienteId) {
+        String queryPedidos = "DELETE FROM pedidos WHERE cliente_id = ?";
+        String queryCliente = "DELETE FROM cliente WHERE id = ?";
+        int filasAfectadas = 0;
+        try {
+            connection.setAutoCommit(false);
+            try (PreparedStatement ps = connection.prepareStatement(queryPedidos)) {
+                ps.setInt(1, clienteId);
+                filasAfectadas += ps.executeUpdate();
+            }
+
+            try (PreparedStatement ps = connection.prepareStatement(queryCliente)) {
+                ps.setInt(1, clienteId);
+                filasAfectadas += ps.executeUpdate();
+            }
+            connection.commit();
+        } catch (SQLException e) {
+            try {
+                connection.rollback();
+            } catch (SQLException ex) {
+                throw new RuntimeException(ex);
+            }
+            throw new RuntimeException();
+        } finally {
+            try {
+                connection.setAutoCommit(true);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("Filas afectadas: " + filasAfectadas);
+        }
+    }
+
+    //actividad 5
+public void totalAcumuladoCliente(String desde, String hasta){
+        String regex = "[1-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])";
+
+        LocalDate fechaInicial = null;
+        LocalDate fechaFinal = null;
+        Scanner scan = new Scanner(System.in);
+        boolean correcto = false;
+        while (!correcto){
+            System.out.println("Introduce la fecha inicial (yyyy-MM-dd)");
+            String fecha = scan.nextLine();
+            if (Pattern.matches(regex, fecha)) {
+                fechaInicial = LocalDate.parse(fecha);
+                System.out.println("Introduce la fecha final (yyyy-MM-dd)");
+                fecha = scan.nextLine();
+                while (!fecha.matches(regex)){
+                    System.out.println("Formato de fecha incorrecto");
+                    fecha = scan.nextLine();
+                }
+                fechaFinal = LocalDate.parse(fecha);
+                correcto = true;
+
+            }
+            else
+                System.out.println("Formato de fecha incorrecto");
+        }
+
+
+        String query = "SELECT c.nombre, COUNT(p.id) as totalPedidos, SUM(p.total) AS Total from cliente c JOIN pedido p ON c.id = p.cliente_id WHERE p.fecha > ? AND p.fecha < ? GROUP BY c.nombre ORDER BY Total DESC, c.nombre ASC";
+
+        try(PreparedStatement ps = connection.prepareStatement(query)){
+            ps.setDate(1, Date.valueOf(fechaInicial));
+            ps.setDate(2, Date.valueOf(fechaFinal));
+            try (ResultSet rs = ps.executeQuery()){
+                while (rs.next()){
+                    ResumenCliente resumenCliente = new ResumenCliente(rs.getString("nombre"), rs.getInt("totalPedidos"), rs.getDouble("Total"));
+                    System.out.println(resumenCliente.toString());
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+}
+
+public void crearIndice(){
+        String query = "CREATE INDEX idx_cliente_fecha ON pedidos(cliente_id, fecha)";
+
+        try (Statement statement = connection.createStatement()){
+            statement.executeUpdate(query);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+}
 
 
     @Override
---
+++ src/dto/ResumenCliente.java
@@ -0,0 +1,4 @@
+package dto;
+
+public record ResumenCliente (String nombre, int numPedidos, double totalGastado) {
+}
